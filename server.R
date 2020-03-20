9b29c80623e33b1e01a8016cb4888ca082baf4ae
maciej <maciej.pomykala@dotecon.com>
2020-03-20 17:35:17 +0000

    Bringing reactive values into the server function so they aren't shared by users

--- server.R
+++ server.R
@@ -9,19 +9,19 @@ source("text.R")
 
 big_text <- h4
 
-state <- reactiveValues(
-  game_status = "inactive",
-  n_players = 0,
-  n_cards = vector("numeric", 0),
-  player_names = vector("character", 0),
-  cards = data.frame(player = numeric(0), value = numeric(0), colour = numeric(0)),
-  history = matrix(NA, nrow = 0, ncol = 4)
-)
-
-ui_elements <- reactiveValues()
-language <- reactiveVal("English")
-
 server <- function(input, output) {
+  state <- reactiveValues(
+    game_status = "inactive",
+    n_players = 0,
+    n_cards = vector("numeric", 0),
+    player_names = vector("character", 0),
+    cards = data.frame(player = numeric(0), value = numeric(0), colour = numeric(0)),
+    history = matrix(NA, nrow = 0, ncol = 4)
+  )
+  
+  ui_elements <- reactiveValues()
+  language <- reactiveVal("English")
+  
   source("server_ui.R", local = TRUE)
   source("server_mechanics.R", local = TRUE)
 }
